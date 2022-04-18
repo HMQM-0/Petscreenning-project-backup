03e3ea36ae3c1e9edd061afd2020ec393975afe6
lauchness <lauchlanchisholm@gmail.com>
2022-04-18 20:13:15 -0300

    Alert Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme, GlobalStyle } from "@styles";
+import { positions, Provider as AlertProvider } from "react-alert";
+import { NotificationTemplate } from "@components/atoms";
+
+const notificationOptions = {
+  position: positions.BOTTOM_RIGHT,
+  timeout: 3000,
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <AlertProvider
+        template={NotificationTemplate as any}
+        {...notificationOptions}
+      >
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AlertProvider>
     </ThemeProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,35 @@
 import type { NextPage } from "next";
+import { useAlert } from "react-alert";
+import styled from "styled-components";
+
+const StyledP = styled.div`
+  background-color: ${({ theme }) => {
+    console.log("theme", theme);
+    return theme.colors.dark;
+  }};
+`;
 
 const Home: NextPage = () => {
-  return <div>UOUOUO</div>;
+  const alert = useAlert();
+
+  return (
+    <>
+      <div>UOUOUO</div>
+      <button
+        onClick={() =>
+          alert.show(
+            {
+              title: "Added 1x pp",
+            },
+            { type: "success" }
+          )
+        }
+      >
+        CLICK
+      </button>
+      <StyledP>Hello World</StyledP>
+    </>
+  );
 };
 
 export default Home;
