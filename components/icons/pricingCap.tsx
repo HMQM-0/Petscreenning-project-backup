e0cac852cf0dcca4526d8c6bf686b8a485096cab
lauchness <lauchlanchisholm@gmail.com>
2022-05-03 23:11:55 -0300

    Moving all images/svg to icon components

---
+++ components/icons/pricingCap.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from "react";
+
+const PricingCap: FunctionComponent = () => {
+  return (
+    <svg
+      viewBox="0 0 256 512"
+      xmlns="http://www.w3.org/2000/svg"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      strokeLinejoin="round"
+      strokeMiterlimit="2"
+    >
+      <path d="M256 512L128 256 256 0H0v512h256z" />
+    </svg>
+  );
+};
+
+export { PricingCap };
