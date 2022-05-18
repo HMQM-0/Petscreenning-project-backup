b5d7d91ffafac98921a9d773a44f7c0088c9a794
lauchness <lauchlanchisholm@gmail.com>
2022-05-18 20:49:54 -0300

    barreling atoms and organisms

---
+++ components/organisms/index.ts
@@ -0,0 +1,7 @@
+export * from "./DropdownSelect";
+export * from "./AddToWishlist";
+export * from "./FilterSidebar";
+export * from "./ProductList";
+export * from "./ProductListHeader";
+export * from "./ProductListItem";
+export * from "./ProductSideNavbar";
