30a0cb8e593953578bb05132197ab7d3970a5a20
lauchness <lauchlanchisholm@gmail.com>
2022-05-18 21:07:13 -0300

    Builder/components/CachedImage refactor

--- components/templates/Builder/components/CachedImage.tsx
+++ components/templates/Builder/components/CachedImage.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-import { PlaceholderImage } from "@components/atoms";
+import { PlaceholderImage } from "components/atoms";
 import { useNetworkStatus } from "@hooks";
-import NoPhoto from "images/no-photo.svg";
-
 import { IImage } from "@types";
 
+// TODO: Refactor this to use next/image
 export const CachedImage: React.FC<IImage> = ({
   url,
   url2x,
   alt,
   children,
-  defaultImage = NoPhoto,
   height,
   width,
   ...props
@@ -20,28 +18,28 @@ export const CachedImage: React.FC<IImage> = ({
   const { online } = useNetworkStatus();
 
   React.useEffect(() => {
-    updateAvailability();
-  }, [online]);
+    async function updateAvailability() {
+      let _isUnavailable = false;
+      if ("caches" in window) {
+        if (!online) {
+          const match = await window.caches.match(url!);
+          let match2x;
+          if (url2x) {
+            match2x = await window.caches.match(url2x);
+          }
+          if (!match && !match2x) {
+            _isUnavailable = true;
+          }
+        }
+      }
 
-  async function updateAvailability() {
-    let _isUnavailable = false;
-    if ("caches" in window) {
-      if (!online) {
-        const match = await window.caches.match(url!);
-        let match2x;
-        if (url2x) {
-          match2x = await window.caches.match(url2x);
-        }
-        if (!match && !match2x) {
-          _isUnavailable = true;
-        }
+      if (isUnavailable !== _isUnavailable) {
+        setUnavailable(_isUnavailable);
       }
     }
 
-    if (isUnavailable !== _isUnavailable) {
-      setUnavailable(_isUnavailable);
-    }
-  }
+    updateAvailability();
+  }, [isUnavailable, online, url, url2x]);
 
   if (!url || isUnavailable) {
     return children || <PlaceholderImage alt={alt} />;
