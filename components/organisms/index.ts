29b0fd9834111f40a09823da78a1d84ae770f3f3
lauchness <lauchlanchisholm@gmail.com>
2022-05-20 16:19:18 -0300

    exporting default for productlistitem

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -3,5 +3,5 @@ export * from "./AddToWishlist";
 export * from "./FilterSidebar";
 export * from "./ProductList";
 export * from "./ProductListHeader";
-export * from "./ProductListItem";
+export { default as ProductListItem } from "./ProductListItem";
 export * from "./ProductSideNavbar";
