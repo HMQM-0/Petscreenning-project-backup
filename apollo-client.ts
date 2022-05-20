64264af1d3f46a859eb96605949f4cdd587a6029
lauchness <lauchlanchisholm@gmail.com>
2022-05-20 10:40:13 -0300

    Adding apollo-client links

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,7 +1,25 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: process.env.NEXT_PUBLIC_API_URI,
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      origin: "http://localhost:3000", // TODO: Use an env variable to ensure this is controlled as expected
+      // authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URI,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
