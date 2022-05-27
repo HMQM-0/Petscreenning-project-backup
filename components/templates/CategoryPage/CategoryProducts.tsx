8155def145def0fdf6fcea403117c7f5cd6a5a30
lauchness <lauchlanchisholm@gmail.com>
2022-05-27 00:46:20 -0300

    Minory TS fixes for new category page query

--- components/templates/CategoryPage/CategoryProducts.tsx
+++ components/templates/CategoryPage/CategoryProducts.tsx
@@ -6,25 +6,20 @@ import { ChildrenFunctionProps } from "components/templates/ProductsList/View";
 import { slugify } from "@utils/core";
 
 type CategoryProductsProps = ChildrenFunctionProps & {
-  category: BasicCategoryFragment
+  category: BasicCategoryFragment;
 };
 
-
 export const extractBreadcrumbs = (category: BasicCategoryFragment) => {
-  const constructLink = (item: { name: string; id: string; }) => ({
-    link: [
-      `/category`,
-      `/${slugify(item.name)}`,
-      `/${item.id}/`,
-    ].join(""),
+  const constructLink = (item: { name: string; id: string }) => ({
+    link: [`/category`, `/${slugify(item.name)}`, `/${item.id}/`].join(""),
     value: item.name,
   });
 
   let breadcrumbs = [constructLink(category)];
 
-  if (category.ancestors?.edges.length) {
-    const ancestorsList = category.ancestors.edges.map((edge) =>
-      constructLink(edge.node)
+  if (category.ancestorList?.categories.length) {
+    const ancestorsList = category.ancestorList.categories.map((edge) =>
+      constructLink(edge.category)
     );
     breadcrumbs = ancestorsList.concat(breadcrumbs);
   }
@@ -36,7 +31,6 @@ const CategoryProducts = ({
   filters,
   category,
 }: CategoryProductsProps) => {
-
   const { loading, data, fetchMore } = useProductsQuery({
     variables: {
       ...variables,
