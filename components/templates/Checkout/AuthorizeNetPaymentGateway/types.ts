eb8d85cf33c585dec47f81a763f21e16111b08fa
lauchness <lauchlanchisholm@gmail.com>
2022-06-02 22:17:37 -0300

    Undeprecating AuthorizeNetPaymentGateway

---
+++ components/templates/Checkout/AuthorizeNetPaymentGateway/types.ts
@@ -0,0 +1,38 @@
+import { CardError } from "core/payments/braintree";
+import { ICardData, IFormError, IPaymentGatewayConfig } from "@types";
+
+import { IFormikProps } from "./cctypes";
+
+export interface IProps {
+  /**
+   * Payment gateway client configuration.
+   */
+  config: IPaymentGatewayConfig[];
+  /**
+   * Form reference on which payment might be submitted.
+   */
+  formRef?: React.RefObject<HTMLFormElement>;
+  /**
+   * Form id on which payment might be submitted.
+   */
+  formId?: string;
+  /**
+   * Errors returned by the payment gateway.
+   */
+  errors?: IFormError[];
+  /**
+   * Postal code used by Braintree.
+   */
+  postalCode?: string;
+  /**
+   * Method called after the form is submitted. Passed token attribute will be used to create payment.
+   */
+  processPayment: (token: string, cardData: ICardData) => void;
+  /**
+   * Method called when gateway error occured.
+   */
+  onError: (errors: IFormError[]) => void;
+}
+
+export type CardErrors = CardError[] | null[];
+export type PropsWithFormik = Omit<IProps, "handleSubmit"> & IFormikProps;
