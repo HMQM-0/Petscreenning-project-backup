6815843c7d902ced9701fbc7c29aae1cdf01cedf
Lauchlan Chisholm <51837850+lauchness@users.noreply.github.com>
2022-06-09 10:16:25 -0300

    Update types/IProduct.ts
    Co-authored-by: Dmitriy Raschepkin <dmitriy.raschepkin@nauticalcommerce.com>

--- types/IProduct.ts
+++ types/IProduct.ts
@@ -1,4 +1,3 @@
-// import { ProductList_products_edges_node } from "@nautical/sdk/lib/queries/gqlTypes/ProductList";
 import { ProductsListProductFragment } from "components/templates/ProductsList/queries.graphql.generated";
 
 type WithOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
