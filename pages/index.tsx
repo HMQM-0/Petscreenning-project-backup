78313e5681f3755837aed7d993e0417ecdeca4a8
lauchness <lauchlanchisholm@gmail.com>
2022-08-04 10:58:23 -0300

    Wrap HomeQuery in try/catch block, add default query response

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,7 @@ import builderConfig from "config/builder";
 import { IndexPage } from "components/templates/IndexPage";
 import { structuredData } from "components/templates/IndexPage/structuredData";
 import { Layout } from "@layouts/Layout";
-import {
-  HomeDocument,
-  HomeQuery,
-} from "components/templates/IndexPage/queries.graphql.generated";
+import { HomeDocument, HomeQuery } from "components/templates/IndexPage/queries.graphql.generated";
 import { getApolloClient } from "apollo-client";
 
 const Home: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ data, builderContent }) => {
@@ -37,12 +34,28 @@ export const getServerSideProps = async () => {
   const client = getApolloClient();
   let content: BuilderContent | null = null;
   if (builderConfig.apiKey) {
-    content = await builder.get("store", { url: "/store/landing" }).promise() || null;
+    content = (await builder.get("store", { url: "/store/landing" }).promise()) || null;
   }
 
-  const { data } = await client.query<HomeQuery>({
-    query: HomeDocument,
-  });
+  let data: HomeQuery = {
+    branding: {
+      id: "",
+      jsonContent: "{}",
+      footerText: "",
+    },
+    shop: {
+      name: "",
+    },
+  };
+
+  try {
+    const { data: response } = await client.query<HomeQuery>({
+      query: HomeDocument,
+    });
+    data = response;
+  } catch (e) {
+    console.log("GraphQL Error: ", e);
+  }
 
   const __APOLLO__: NormalizedCacheObject = client.extract();
 
