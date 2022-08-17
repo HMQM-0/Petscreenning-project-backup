b43dcbda17abada90ac60e191a46722aa9300767
lauchness <lauchlanchisholm@gmail.com>
2022-08-16 21:05:50 -0300

    [PROD-2215] refactoring useHandlerWhenClickedOutside hook

--- deprecated/@next/hooks/index.ts
+++ deprecated/@next/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from "./useLocalStorage";
 
-export * from "./useHandlerWhenClickedOutside";
 export * from "./useNetworkStatus";
 export * from "./useCheckoutStepState";
 export * from "./useCheckoutStepFromPath";
