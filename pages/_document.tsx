f9ac3e14dfb8e95ae82b058c6d6b0ec1ffd84fcc
lauchness <lauchlanchisholm@gmail.com>
2022-08-30 21:50:41 -0300

    [PROD-2380] fixing SSR styled components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,42 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-// It is likely that you do NOT want to modify this page
-// Check here before modifying this document: https://nextjs.org/docs/advanced-features/custom-document
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <link rel="manifest" href="/manifest.json" id="manifest" />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head />
+        <link rel="manifest" href="/manifest.json" id="manifest" />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
