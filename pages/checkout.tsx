167ff247f7ea3dbdbd7695d6662d6ddb736b8ac0
lauchness <lauchlanchisholm@gmail.com>
2022-09-01 23:38:27 -0300

    [PROD-2380] checkout page document head generated in getServerSideProps

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,4 +1,4 @@
-import type { NextPage, InferGetServerSidePropsType } from "next";
+import type { NextPage, InferGetServerSidePropsType, GetServerSidePropsContext } from "next";
 import { NormalizedCacheObject } from "@apollo/client";
 import { useRouter } from "next/router";
 import { useQueryParams, BooleanParam, StringParam } from "next-query-params";
@@ -9,33 +9,22 @@ import { Layout } from "@layouts/Layout";
 import { CheckoutPageDocument, CheckoutPageQuery } from "components/templates/CheckoutPage/queries.graphql.generated";
 import { CheckoutPage } from "components/templates/CheckoutPage";
 import { Logo } from "components/atoms/Logo";
-import { getPayment, IS_SSR } from "utils";
+import { getPayment, getSeoURL, IS_SSR } from "utils";
+import { DocumentHead } from "types";
 
 import { getApolloClient } from "../apollo-client";
 
-const Checkout: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ data }) => {
+const Checkout: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ data, documentHead }) => {
   const { push } = useRouter();
   const { user } = useAuth();
   const { loaded, lines } = useCheckout();
+  const payment = getPayment();
 
   const [{ payment_intent, payment_intent_client_secret, guest }] = useQueryParams({
     payment_intent: StringParam,
     payment_intent_client_secret: StringParam,
     guest: BooleanParam,
   });
-  const URL = IS_SSR ? "" : location.href;
-  const payment = getPayment();
-  const description = data?.shop.description ?? "";
-  const title = data?.shop.name ?? "";
-  const schema = structuredData(description, title, URL);
-  const documentHead = {
-    branding: data.branding,
-    description,
-    title,
-    schema,
-    image: data.branding?.logo ?? "",
-    url: "", // TODO: Store the canonical URL either as env or in dasboard
-  };
 
   const isFinalizingPayment = (payment_intent && payment_intent_client_secret) || payment?.token;
   const redirectToCart = !isFinalizingPayment && loaded && (!lines || lines.length === 0);
@@ -57,7 +46,7 @@ const Checkout: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>>
   );
 };
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const client = getApolloClient();
 
   const { data } = await client.query<CheckoutPageQuery>({
@@ -66,9 +55,24 @@ export const getServerSideProps = async () => {
 
   const __APOLLO__: NormalizedCacheObject = client.extract();
 
+  const url = getSeoURL(context);
+
+  const description = data?.shop.description ?? "";
+  const title = data?.shop.name ?? "";
+  const schema = structuredData(description, title, url);
+  const documentHead: DocumentHead = {
+    branding: data.branding,
+    description,
+    title,
+    schema,
+    image: data.branding?.logo?.url ?? "",
+    url,
+  };
+
   return {
     props: {
       data,
+      documentHead,
       __APOLLO__,
     },
   };
