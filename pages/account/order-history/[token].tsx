c281d67dda90ee48ba8e5e3f7e48f86ed5a4720b
lauchness <lauchlanchisholm@gmail.com>
2022-09-02 00:18:29 -0300

    [PROD-2380] document head generation in server side props for order history detail page

--- pages/account/order-history/[token].tsx
+++ pages/account/order-history/[token].tsx
@@ -1,32 +1,20 @@
 import { GetServerSidePropsContext } from "next";
 import type { NextPage, InferGetServerSidePropsType } from "next";
 import { NormalizedCacheObject } from "@apollo/client";
+
 import {
   OrderHistoryDetailsPageDocument,
   OrderHistoryDetailsPageQuery,
 } from "components/templates/OrderHistoryDetailsPage/queries.graphql.generated";
-
 import { OrderHistoryDetailsPage } from "components/templates/OrderHistoryDetailsPage";
 import NotFound from "components/molecules/NotFound";
 import { AccountSettingsLayout } from "@layouts/AccountSettingsLayout";
 import { structuredData } from "components/templates/IndexPage/structuredData";
 import { Layout } from "@layouts/Layout";
 import { getSsrApolloClient } from "apollo-client";
-import { IS_SSR } from "utils";
+import { getSeoURL } from "utils";
 
-const OrderHistory: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ token, data }) => {
-  const URL = IS_SSR ? "" : location.href;
-  const description = "Order History Details";
-  const title = "Order";
-  const schema = structuredData(description, title, URL);
-  const documentHead = {
-    branding: data.branding,
-    description,
-    title,
-    schema,
-    url: "",
-  };
-
+const OrderHistory: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ token, documentHead }) => {
   if (!token) {
     return <NotFound />;
   }
@@ -51,10 +39,22 @@ export const getServerSideProps = async (context: GetServerSidePropsContext<{ to
 
   const __APOLLO__: NormalizedCacheObject = client.extract();
 
+  const url = getSeoURL(context);
+  const description = "Order History Details";
+  const title = "Order";
+  const schema = structuredData(description, title, url);
+  const documentHead = {
+    branding: data.branding,
+    description,
+    title,
+    schema,
+    url,
+  };
+
   return {
     props: {
       token,
-      data,
+      documentHead,
       __APOLLO__,
     },
   };
