c24d83489ec8483466a7cc19488cbea9eeadb4d9
lauchness <lauchlanchisholm@gmail.com>
2022-09-02 10:56:38 -0300

    [PROD-2380] remove  from structured data for index page

--- components/templates/IndexPage/structuredData.ts
+++ components/templates/IndexPage/structuredData.ts
@@ -1,18 +1,9 @@
-// Remove url-join package - use URL instead
-import urljoin from "url-join";
-
 export const structuredData = (description: string, name: string, url: string) => {
-  const _url = url || "";
   return JSON.stringify({
     "@context": "https://schema.org",
     "@type": "WebSite",
     description: description,
     name: name,
-    potentialAction: {
-      "@type": "SearchAction",
-      "query-input": "required name=q",
-      target: urljoin(_url, "search", "?q={q}"),
-    },
-    url: _url,
+    url,
   });
 };
