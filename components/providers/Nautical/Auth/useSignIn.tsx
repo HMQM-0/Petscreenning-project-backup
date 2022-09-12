38e7cd8d494b1f189468ea660dfe10ad3cc6e840
lauchness <lauchlanchisholm@gmail.com>
2022-09-12 19:08:34 -0300

    [PROD-2311] refactoring useSignIn hook to update provider fetching/error state via dispatch

--- components/providers/Nautical/Auth/useSignIn.tsx
+++ components/providers/Nautical/Auth/useSignIn.tsx
@@ -1,10 +1,11 @@
 import React, { useCallback } from "react";
+import { useLazyQuery } from "@apollo/client";
 
 import { setCsrfToken, setSignInToken } from "utils";
 import { IS_SSR } from "utils/isSSR";
 
 import { useSignInMutation } from "./mutations.graphql.generated";
-import { useUserDetailsLazyQuery } from "./queries.graphql.generated";
+import { UserDetailsDocument } from "./queries.graphql.generated";
 import { AuthActionCreators, AuthActions } from "./actions";
 import { BROWSER_NO_CREDENTIAL_API_MESSAGE } from "./constants";
 
@@ -14,8 +15,9 @@ type useSignInProps = {
 
 const useSignIn = ({ dispatch }: useSignInProps) => {
   const [signInMutation] = useSignInMutation({ fetchPolicy: "no-cache" });
-  const [userDetailsQuery] = useUserDetailsLazyQuery({
+  const [userDetailsQuery] = useLazyQuery(UserDetailsDocument, {
     fetchPolicy: "network-only",
+    errorPolicy: "all",
   });
   return useCallback(
     /**
@@ -25,50 +27,56 @@ const useSignIn = ({ dispatch }: useSignInProps) => {
      * @param autoSignIn Indicates if SDK should try to sign in user with given credentials in future without explicitly calling this method. True by default.
      */
     async (email: string, password: string, autoSignIn: boolean = true) => {
-      const { data, errors: graphqlErrors } = await signInMutation({
-        variables: { email, password },
-      });
-
-      const errors = data?.tokenCreate?.errors || graphqlErrors || [];
-
+      dispatch(AuthActionCreators.fetching());
       try {
-        // @ts-ignore
-        if (!IS_SSR && autoSignIn && !errors.length && window.PasswordCredential) {
+        const { data, errors: graphqlErrors } = await signInMutation({
+          variables: { email, password },
+        });
+
+        const errors = data?.tokenCreate?.errors || graphqlErrors || [];
+
+        try {
           // @ts-ignore
-          const passwordCredential = new window.PasswordCredential({
-            id: email,
-            password,
-          });
-          await navigator.credentials.store(passwordCredential);
+          if (!IS_SSR && autoSignIn && !errors.length && window.PasswordCredential) {
+            // @ts-ignore
+            const passwordCredential = new window.PasswordCredential({
+              id: email,
+              password,
+            });
+            await navigator.credentials.store(passwordCredential);
+          }
+        } catch (credentialsError) {
+          console.warn(BROWSER_NO_CREDENTIAL_API_MESSAGE, credentialsError);
         }
-      } catch (credentialsError) {
-        console.warn(BROWSER_NO_CREDENTIAL_API_MESSAGE, credentialsError);
+
+        if (errors.length) {
+          dispatch(AuthActionCreators.errors(errors.map((error) => ({ message: error.message }))));
+          return;
+        }
+
+        setSignInToken(data?.tokenCreate?.token || null);
+        setCsrfToken(data?.tokenCreate?.csrfToken || null);
+
+        const { data: userData, error: userDataError } = await userDetailsQuery({
+          onCompleted: () => {
+            if (userDataError) {
+              dispatch(AuthActionCreators.errors([userDataError]));
+              return;
+            }
+
+            const token = data?.tokenCreate?.token ?? undefined;
+            const user = userData?.me;
+
+            dispatch(AuthActionCreators.signIn(token, user));
+          },
+          onError: (apolloError) => {
+            dispatch(AuthActionCreators.errors([apolloError]));
+          },
+        });
+      } catch {
+        const errors = [{ message: "There was an unexpected error. Please try again", field: "password" }];
+        dispatch(AuthActionCreators.errors(errors));
       }
-
-      if (errors.length) {
-        return {
-          errors,
-        };
-      }
-
-      setSignInToken(data?.tokenCreate?.token || null);
-      setCsrfToken(data?.tokenCreate?.csrfToken || null);
-
-      const { data: userData, error: userDataError } = await userDetailsQuery();
-
-      if (userDataError) {
-        return {
-          errors: [userDataError],
-        };
-      }
-
-      const token = data?.tokenCreate?.token ?? undefined;
-      const user = userData?.me;
-      dispatch(AuthActionCreators.signIn(token, user));
-
-      return {
-        errors: null,
-      };
     },
     [dispatch, signInMutation, userDetailsQuery],
   );
