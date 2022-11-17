28f690ca4529fea55c785c71e14abf0b93a1d161
jleonelliPeak <jleonelli@peakactivity.com>
2022-11-17 14:39:14 -0300

    Adding rokt Payments marketplace sdk

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,21 @@ style="border: 0px; width: 100%; display: none;"
 
 const RoktIframeScript = () => <noscript dangerouslySetInnerHTML={{ __html: ROKT_IFRAME }} />;
 
+export const PAYMENTS_MARKETPLACE_SCRIPT = `(function(r,o,k,t,n,e,w,a,){r._ROKT=n;r[n]=r[n]||{id:t,h:w,lc:[],it:new
+  Date(),onLoaded:function(c)
+  {r[n].lc.push(c)}};a=o.createElement('script');a.type='text/javascript';a.async=!0;a.src=k;if(e)
+  {a.integrity=e;a.crossOrigin='anonymous'}=o.getElementsByTagName('script')
+  [0];.parentNode.insertBefore(a,_)})
+  (window,document,'https://apps.rokt.com/wsdk/integrations/snippet.js','3071804547766951791',
+  'rokt');`;
+
+const PaymentsMarketplaceScript = () => (
+  <script
+    dangerouslySetInnerHTML={{ __html: PAYMENTS_MARKETPLACE_SCRIPT }}
+    id="pm-script"
+  />
+);
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
@@ -48,6 +63,7 @@ export default class MyDocument extends Document {
             type="text/javascript"
             src="https://apps.rokt.com/wsdk/integrations/launcher.js"
           />
+          <PaymentsMarketplaceScript />
         </Head>
         <link
           rel="manifest"
